41e2f0a077ab407628b3485e424c34d1fc286a6e
Leander Seige <leander@seige.name>
2020-12-08 22:15:52 +0100

    text changed

--- 2020/imagenviewer/src/App.js
+++ 2020/imagenviewer/src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <img src={logo} alt="logo" />
       <p>ImageN is an image generator that uses a single integer with an accuracy of 65535 bits to generate every possible image. The generator initialized the integer with the value 0 and now constantly adds the value 1 in order to generate every possible pattern and thus every possible image.</p>
-      <p>ImageN is an art project that works with the opposites of calculability and complexity.</p>
+      <p>ImageN is an art project that works with the contrast of computability and complexity.</p>
       <Viewer />
       <p>The generator is a tiny C programm calculating millions of patterns per second. The original interface was a Java applet with many options to turn the pattern into images. Currently I can only provide a simple Javascript replacement. I hope to improve it in the future. The raw data can be accessed at <a href="http://imagen.manducus.net/seed">http://imagen.manducus.net/seed</a>.</p>
       <p>contemporary and historic source codes can be found on <a href="https://github.com/leanderseige/imagen" target="_blank">https://github.com/leanderseige/imagen</a></p>
